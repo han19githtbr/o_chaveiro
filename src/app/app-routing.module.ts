008f7ec85571956a7d09d4d44286065913977e13
han19githtbr <milliance23@gmail.com>
2024-09-27 17:03:27 -0300

    atualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import GerencialComponent from './modules/gerencial/gerencial.component';
 import HomeComponent from './modules/home/home.component';
-import CriarContaComponent from './modules/criar-conta/criar-conta.component';
 
 const routes: Routes = [
   {
@@ -15,14 +14,7 @@ const routes: Routes = [
     loadChildren: () => import('../app/modules/home/home.routes'),
     component: HomeComponent
   },
-  {
-    path: 'criar-conta',
-    loadComponent: () => import('../app/modules/criar-conta/criar-conta.component'),
-  },
-  /*{
-    path: 'criar-conta',
-    loadComponent: () => import('./modules/home/criar-conta/criar-conta.component'),
-  },*/
+  
   {
     path: 'gerencial',
     loadChildren: () => import('../app/modules/gerencial/gerencial.routes'),
